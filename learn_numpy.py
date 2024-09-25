076375c9eb41b1f4a64b8c9aa5275c6927f4cc0d
Islam-Almshamsheh <148493833+Islam-Almshamsheh@users.noreply.github.com>
2024-09-25 23:27:41 +0300

    Add files via upload

---
+++ learn_numpy.py
@@ -0,0 +1,37 @@
+import numpy as np, sys, time
+
+
+# comparing the performance
+elements = 150
+my_list1 = range(elements)
+my_list2 = range(elements)
+
+my_array1 = np.arange(elements) 
+my_array2 = np.arange(elements) 
+list_start = time.time()
+list_result = [(n1 + n2) for n1,n2 in zip(my_list1,my_list2)]
+print(f"List Time: {time.time() - list_start}")
+
+array_start = time.time()
+array_result = my_array1 + my_array2
+print(f"List Time: {time.time() - array_start}")
+
+print("#" * 50)
+
+# comparing memory usage
+my_array = np.arange(100)
+print(my_array)
+print(my_array.itemsize)
+print(my_array.size)
+print(f"All Bytes: {my_array.itemsize* my_array.size}")
+
+my_list = range(100)
+print(sys.getsizeof(1))
+print(len(my_list))
+print(f"All Bytes: {sys.getsizeof(1)* len(my_list)}")
+
+print("#" * 50)
+d = np.array([['A','B','X'],['C','D','Y'],['M','N','O']])
+print(d[50:100]) # output=> []
+# NumPy allows you to slice arrays beyond their actual size without raising an IndexError.
+print(d[1: ,0])
